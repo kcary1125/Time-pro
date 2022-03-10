4d6a9161c117de7979f945df8a92e58616995f26
dgrant6 <dgran013@plattsburgh.edu>
2022-03-09 21:04:54 -0500

    test git

--- timepro/lib/main.dart
+++ timepro/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget{
       debugShowCheckedModeBanner: false,
         home: AnimatedSplashScreen(
         duration: 3000,
-        splash: Image.asset('assets/timesmall.png'),
+        splash: Image.asset('assets/time.png'),
         nextScreen: LoginPage(),
         splashTransition: SplashTransition.scaleTransition,
         backgroundColor: Colors.deepPurple,
